202925bdc4cfbffa4a719acc9b9ef123b5ccb5e5
Aleksandr Nyatin <qaleks.test@gmail.com>
2019-10-02 12:50:09 +0500

    Выполнено упражнение 4-го урока

--- index.php
+++ index.php
@@ -13,27 +13,59 @@
 (проверка деления на ноль)
 </p>
 
-<form action="/index.php" method "post">
-<input type="text" name="Введичи число №1" />
+<form action="/index.php" method="post">
+<input type="text" name="num1" />
 <select name="form_select">
-    <option value="Сложение">+</option>
-    <option value="Вычитание">-</option>
-    <option value="Умножение">*</option>
-    <option value="Деление">/</option>
+    <option value="plus">+</option>
+    <option value="minus">-</option>
+    <option value="*">*</option>
+    <option value="/">/</option>
   </select>
-<input type="text" name="Введичи число №2" />
-<input type="submit" value="Ответ" />
+<input type="text" name="num2" />
+<input type="submit" value="Показать ответ" />
 <br>
-<input type="reset" name="Очистить" />
-
+<input type="reset" value="Очистить" />
 
 </form>
 <?php
 
+$a1=$_POST["num1"];
+$a2=$_POST["num2"];
 
+//echo var_dump($a1) . '<br>';
+//echo var_dump($a2) . '<br>';
+//echo $a1+$a2;
+
+if (is_numeric($a1) && is_numeric($a2)) {
+
+if ($_POST["form_select"]=="plus"){
+	echo 'Сумма чисел = ';
+	echo $a1+$a2;	
+}
+
+elseif ($_POST["form_select"]=="minus"){
+	echo 'Разность = ';
+	echo $a1-$a2;
+}
+
+elseif ($_POST["form_select"]=="*"){
+	echo 'Результат умножения чисел = ';
+	echo $a1*$a2;	
+}
+
+elseif ($_POST["form_select"]=="/"){
+	if (0==$a2){echo 'На ноль делить нельзя! Введите другое число';}
+	else{
+		echo 'Результат деления чисел = ';
+	    echo $a1/$a2;
+	}
+	
+}
+
+}
+else echo "Допускается ввод только числе!";
 
 ?> 
-
 
 </body>
 
